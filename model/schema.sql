661ae92d49a569f392b3dd74cfcbe7a3f4ea5914
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-02-22 17:46:33 -0800

    added new table and columns to schema

--- model/schema.sql
+++ model/schema.sql
@@ -1,11 +1,24 @@
 DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 USE bamazon;
+
 CREATE TABLE products(
     item_id INTEGER(11) NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
     price DECIMAL(6,2) NOT NULL,
     stock_quantity INTEGER(6) NOT NULL,
+    product_sales INTEGER(10) NOT NULL,
     PRIMARY KEY(item_id)
  );       
+
+
+ CREATE TABLE departments(
+    department_id INTEGER(11) NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    over_head_costs INTEGER(6) NOT NULL,
+    PRIMARY KEY(department_id) 
+ );
+
+
+
--- model/seeds.sql
+++ model/seeds.sql
@@ -1,20 +1,33 @@
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Headphone Amp", "Electronics", 45.00, 27);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Ginsu Knife", "Kitchen", 19.95, 17);
-INSERT INTO products( product_name, department_name, price,stock_quantity)
-    VALUES("Hi-Fi Portable Music Player", "Electronics", 745.00, 13);        
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Headphones", "Electronics", 145.00, 33);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Insta Pot", "Kitchen", 125.00, 7);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Cuisinart", "Kitchen", 155.00, 29);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("B-Complex Vitamin", "Health", 15.95, 27);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Collagen Peptides", "Health", 22.00, 27);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Host Defense", "Health", 35.00, 27);
-INSERT INTO products(product_name, department_name, price,stock_quantity)
-    VALUES("Turntable", "Electronics", 245.00, 23);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Headphone Amp", "Electronics", 45.00, 27, 0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Ginsu Knife", "Kitchen", 19.95, 17,0);
+INSERT INTO products( product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Hi-Fi Portable Music Player", "Electronics", 745.00, 13,0);        
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Headphones", "Electronics", 145.00, 33,0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Insta Pot", "Kitchen", 125.00, 7,0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Cuisinart", "Kitchen", 155.00, 29,0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("B-Complex Vitamin", "Health", 15.95, 27,0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Collagen Peptides", "Health", 22.00, 27,0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Host Defense", "Health", 35.00, 27,0);
+INSERT INTO products(product_name, department_name, price,stock_quantity,product_sales)
+    VALUES("Turntable", "Electronics", 245.00, 23,0);
+
+
+
+
+
+
+INSERT INTO departments(department_name,over_head_costs) 
+        VALUES("Health", 1200);    
+INSERT INTO departments(department_name,over_head_costs) 
+        VALUES("Kitchen", 1800);  
+INSERT INTO departments(department_name,over_head_costs) 
+        VALUES("Electronics", 1500);  
+        
